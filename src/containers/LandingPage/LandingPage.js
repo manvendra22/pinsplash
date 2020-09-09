0190dbbc6f12065830e3525eb3a8c39c2d7f08cf
Manvendra Singh <mjdeora@gmail.com>
2020-09-10 00:43:47 +0530

    :sparkles: New library for masonry

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import Masonry from 'react-masonry-css'
+// import Masonry from 'react-masonry-css'
+import StackGrid from "react-stack-grid";
 
 import "./LandingPage.scss"
 
 import Header from '../../components/Header/Header'
 import ImageBox from '../../components/ImageBox/ImageBox'
 
-
 export default function LandingPage() {
     return (
         <main>
             <Header />
-            <Masonry
-                breakpointCols={3}
-                className="my-masonry-grid"
-                columnClassName="my-masonry-grid_column"
+            <StackGrid
+                columnWidth={300}
+                gutterWidth={20}
+                gutterHeight={30}
+                className="image-grid"
             >
                 <ImageBox url="https://dummyimage.com/300x400" />
                 <ImageBox url="https://dummyimage.com/300x200" />
@@ -26,7 +27,7 @@ export default function LandingPage() {
                 <ImageBox url="https://dummyimage.com/300x500" />
                 <ImageBox url="https://dummyimage.com/300x600" />
                 <ImageBox url="https://dummyimage.com/300x400" />
-            </Masonry>
+            </StackGrid>
         </main>
     )
 }
