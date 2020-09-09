9ae71d7fa45e26ab02b81a1861f255d3f72cdc51
Manvendra Singh <mjdeora@gmail.com>
2020-09-09 13:14:18 +0530

    :art: Update code structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
+
+import Landing from './containers/Landing'
+import Search from './containers/Search'
+import Images from './containers/Images'
 
 function App() {
   return (
     <div className="App">
-      Image Explorer
+      <Router>
+        {/* <Switch> */}
+        <Route path="/">
+          <Landing />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/images">
+          <Images />
+        </Route>
+        {/* </Switch> */}
+      </Router>
     </div>
   );
 }
