01a17496a9e2b8b1bfc6971a1ed97c2f1b5e1864
Manvendra Singh <mjdeora@gmail.com>
2020-09-11 00:08:20 +0530

    :lipstick:  Image details styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,14 @@ export default function Header({ searchImage }) {
     return (
         <header>
             <div className="searchbar">
-                <input type="text" placeholder="Search here" value={value} onChange={e => setValue(e.target.value)} />
+                <div className="searchContainer">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <circle cx="10" cy="10" r="7" />
+                        <line x1="21" y1="21" x2="15" y2="15" />
+                    </svg>
+                    <input type="text" placeholder="Search here" value={value} onChange={e => setValue(e.target.value)} />
+                </div>
                 <button className="btn btn-primary" onClick={handleClick}>Search</button>
             </div>
         </header>
--- src/containers/ImageDetails/ImageDetails.js
+++ src/containers/ImageDetails/ImageDetails.js
@@ -30,7 +30,7 @@ export default function ImageDetails() {
                 <div className="info">
                     <div>
                         <div className="header">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <circle cx="12" cy="12" r="2" />
                                 <path d="M2 12l1.5 2a11 11 0 0 0 17 0l1.5 -2" />
@@ -42,7 +42,7 @@ export default function ImageDetails() {
                     </div>
                     <div>
                         <div className="header">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <path d="M7 11v 8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" />
                             </svg>
@@ -52,7 +52,7 @@ export default function ImageDetails() {
                     </div>
                     <div>
                         <div className="header">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
                                 <polyline points="7 11 12 16 17 11" />
@@ -65,8 +65,26 @@ export default function ImageDetails() {
                     {/* <div>{data.description}</div> */}
                 </div>
                 <div className="btns">
-                    <button className="btn btn-secondary">Download</button>
-                    <button className="btn btn-tertiary">Share</button>
+                    <button className="btn btn-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                            <polyline points="7 11 12 16 17 11" />
+                            <line x1="12" y1="4" x2="12" y2="16" />
+                        </svg>
+                        Download
+                    </button>
+                    <button className="btn btn-tertiary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <circle cx="6" cy="12" r="3" />
+                            <circle cx="18" cy="6" r="3" />
+                            <circle cx="18" cy="18" r="3" />
+                            <line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
+                            <line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
+                        </svg>
+                        Share
+                    </button>
                 </div>
             </section>
         </main>
