47385b77126ab80433b149f8457c2858c5acc6e4
Manvendra Singh <mjdeora@gmail.com>
2020-09-11 21:42:50 +0530

    :art: Minor refactor

--- src/assets/Share.js
+++ src/assets/Share.js
@@ -4,8 +4,6 @@ function Icon({ className, color }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       fill="none"
       stroke={color}
       className={className}
