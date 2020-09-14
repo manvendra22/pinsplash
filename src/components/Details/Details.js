982488b3113b5c2ad77f80fe767a40cba45bc8aa
Manvendra Singh <mjdeora@gmail.com>
2020-09-14 20:57:53 +0530

    :art: Component structure improved

---
+++ src/components/Details/Details.js
@@ -0,0 +1,104 @@
+import React, { useRef } from 'react'
+import cogoToast from 'cogo-toast';
+
+import './Details.scss'
+
+import { triggerDownload } from '../../utility/query'
+
+import Like from '../../assets/Like'
+import View from '../../assets/View'
+import Share from '../../assets/Share'
+import Download from '../../assets/Download'
+import Location from '../../assets/Location'
+
+export default function Details({ data: { urls, links, views, likes, downloads, location, description } }) {
+    const downloadButton = useRef()
+
+    function onDownloadProgress(progressEvent) {
+        const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+        if (downloadButton.current) {
+            downloadButton.current.firstChild.style.width = `${progress}%`
+        }
+    }
+
+    function downloadComplete() {
+        if (downloadButton.current) {
+            downloadButton.current.disabled = false;
+            downloadButton.current.style.background = null;
+            downloadButton.current.firstChild.style.width = '0%'
+        }
+    }
+
+    function downloadImage() {
+        downloadButton.current.disabled = true;
+        downloadButton.current.style.background = '#6b77e1';
+        triggerDownload(urls.full, links.download_location, onDownloadProgress, downloadComplete)
+    }
+
+
+    return (
+        <div className="details">
+            <div className="stats">
+                <div className="stat">
+                    <div className="icon-container">
+                        <View className="icon" color="#2c3e50" />
+                                Views
+                            </div>
+                    <div className="value">{views}</div>
+                </div>
+                <div className="stat">
+                    <div className="icon-container">
+                        <Like className="icon" color="#2c3e50" />
+                                Likes
+                            </div>
+                    <div className="value">{likes}</div>
+                </div>
+                <div className="stat">
+                    <div className="icon-container">
+                        <Download className="icon" color="#2c3e50" />
+                                Downloads
+                            </div>
+                    <div className="value">{downloads}</div>
+                </div>
+            </div>
+            <div className="description">
+                {
+                    (location.title || location.city) &&
+                    <div className="icon-container">
+                        <Location className="icon" color="#2c3e50" />
+                        <span className="address">{location.title || location.city}</span>
+                    </div>
+                }
+                {
+                    description && <div>{description}</div>
+                }
+            </div>
+            <div className="btns">
+                <button ref={downloadButton} className="btn btn-secondary icon-container" onClick={downloadImage}>
+                    <span className="progress"></span>
+                    <Download className="icon" color="#fff" />
+                        Download
+                    </button>
+                <button className="btn btn-tertiary icon-container" onClick={shareImage}>
+                    <Share className="icon" color="#fff" />
+                        Share
+                    </button>
+            </div>
+        </div>
+    )
+}
+
+function shareImage() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Image',
+            text: 'Check out this image',
+            url: window.location.href
+        })
+            .then(() => cogoToast.success("Shared!", { position: 'top-right' }))
+            .catch((error) => cogoToast.error(error, { position: 'top-right' }));
+    } else {
+        navigator.clipboard.writeText(window.location.href)
+        cogoToast.success("Copied!", { position: 'top-right' });
+    }
+}
--- src/containers/ImageDetails/ImageDetails.js
+++ src/containers/ImageDetails/ImageDetails.js
@@ -1,25 +1,19 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
-import cogoToast from 'cogo-toast';
 
 import "./ImageDetails.scss"
 
-import { useImage, triggerDownload } from '../../utility/query'
+import { useImage } from '../../utility/query'
 
-import Like from '../../assets/Like'
-import View from '../../assets/View'
-import Share from '../../assets/Share'
-import Download from '../../assets/Download'
-import Location from '../../assets/Location'
 
 import Loader from '../../components/Loader/Loader'
+import Details from '../../components/Details/Details'
 
 const dpr = window.devicePixelRatio;
 
 export default function ImageDetails() {
     let { id } = useParams();
     const { status, data, error } = useImage(id);
-    const downloadButton = useRef()
 
     if (status === "loading") {
         return <Loader />
@@ -29,33 +23,12 @@ export default function ImageDetails() {
         return <div className="error">Error: {error.message}</div>
     }
 
-    function onDownloadProgress(progressEvent) {
-        const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-        if (downloadButton.current) {
-            downloadButton.current.firstChild.style.width = `${progress}%`
-        }
-    }
-
-    function downloadComplete() {
-        if (downloadButton.current) {
-            downloadButton.current.disabled = false;
-            downloadButton.current.style.background = null;
-            downloadButton.current.firstChild.style.width = '0%'
-        }
-    }
-
-    function downloadImage() {
-        downloadButton.current.disabled = true;
-        downloadButton.current.style.background = '#6b77e1';
-        triggerDownload(data.urls.full, data.links.download_location, onDownloadProgress, downloadComplete)
-    }
-
     let heightT = 600
     let widthT = 600
     let innerWidth = window.innerWidth
     innerWidth = innerWidth - (innerWidth * 10 / 100)
 
-    const { urls, views, likes, downloads, location, description, color, width, height } = data
+    const { urls, color, width, height } = data
 
     widthT = width / height * heightT;
 
@@ -69,69 +42,7 @@ export default function ImageDetails() {
             <div className="image" style={{ background: color, height: heightT, width: widthT }}>
                 <img alt="" src={`${urls.raw}q=75&auto=format&h=${heightT}&dpr=${dpr}&fit=max`} />
             </div>
-            <div className="details">
-                <div className="stats">
-                    <div className="stat">
-                        <div className="icon-container">
-                            <View className="icon" color="#2c3e50" />
-                                Views
-                            </div>
-                        <div className="value">{views}</div>
-                    </div>
-                    <div className="stat">
-                        <div className="icon-container">
-                            <Like className="icon" color="#2c3e50" />
-                                Likes
-                            </div>
-                        <div className="value">{likes}</div>
-                    </div>
-                    <div className="stat">
-                        <div className="icon-container">
-                            <Download className="icon" color="#2c3e50" />
-                                Downloads
-                            </div>
-                        <div className="value">{downloads}</div>
-                    </div>
-                </div>
-                <div className="description">
-                    {
-                        (location.title || location.city) &&
-                        <div className="icon-container">
-                            <Location className="icon" color="#2c3e50" />
-                            <span className="address">{location.title || location.city}</span>
-                        </div>
-                    }
-                    {
-                        description && <div>{description}</div>
-                    }
-                </div>
-                <div className="btns">
-                    <button ref={downloadButton} className="btn btn-secondary icon-container" onClick={downloadImage}>
-                        <span className="progress"></span>
-                        <Download className="icon" color="#fff" />
-                        Download
-                    </button>
-                    <button className="btn btn-tertiary icon-container" onClick={shareImage}>
-                        <Share className="icon" color="#fff" />
-                        Share
-                    </button>
-                </div>
-            </div>
+            <Details data={data} />
         </div>
     )
-}
-
-function shareImage() {
-    if (navigator.share) {
-        navigator.share({
-            title: 'Image',
-            text: 'Check out this image',
-            url: window.location.href
-        })
-            .then(() => cogoToast.success("Shared!", { position: 'top-right' }))
-            .catch((error) => cogoToast.error(error, { position: 'top-right' }));
-    } else {
-        navigator.clipboard.writeText(window.location.href)
-        cogoToast.success("Copied!", { position: 'top-right' });
-    }
 }
